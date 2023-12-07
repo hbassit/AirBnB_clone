8f17f518519eec8c0a02f47b4f57a977840a25d1
Haqq Bassit <haqq.bm@gmail.com>
2023-12-07 17:21:09 +0000

    update base model to enable creation of object with dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -11,10 +11,19 @@ class BaseModel:
     Defines all common attributes and methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Function initializing base object
         """
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
