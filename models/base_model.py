c797d3d5e225a8db72631e2f2f70e9a5c9115a19
Haqq Bassit <haqq.bm@gmail.com>
2023-12-10 23:28:55 +0000

    add FileStorage class

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ Module containing Base Model class
 """
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """
@@ -15,7 +15,7 @@ class BaseModel:
         """
         Function initializing base object
         """
-        if len(kwargs) > 0:
+        if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
@@ -27,6 +27,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        models.storage.new(self)
 
     def __str__(self):
         """
@@ -39,6 +40,7 @@ class BaseModel:
         Updates public instance attribute "updated_at" with current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
